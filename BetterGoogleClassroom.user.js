beb1aaa6c9b656632d7a507310c71c926669c61d
era2010-coder <72707293+era2010-coder@users.noreply.github.com>
2021-05-20 11:06:10 -0400

    Update BetterGoogleClassroom.user.js

--- BetterGoogleClassroom.user.js
+++ BetterGoogleClassroom.user.js
@@ -3,9 +3,13 @@
 // @namespace   awesome-userscripts.github.io
 // @match       *://classroom.google.com/*
 // @grant       none
-// @version     1.0
+// @version     2.0
 // @author      era2010-coder
 // @description Userscript to make google classroom better
 // ==/UserScript==
 
-console.log("I detect google classroom")
+var css = "body { background: whitesmoke; }"
+var style = document.createElement("style");
+style.type = "text/css";
+style.appendChild(document.createTextNode(css));
+document.head.appendChild(style);
