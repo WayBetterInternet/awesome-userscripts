b0be9d0e5cc255e9eee737c67e02f803f8abfcf5
era2010-coder <72707293+era2010-coder@users.noreply.github.com>
2021-05-20 12:36:26 -0400

    Update BetterGoogle.user.js

--- BetterGoogle.user.js
+++ BetterGoogle.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        Better Google
 // @namespace   awesome-userscripts.github.io
-// @match       *://google.*/*
+// @match       *://www.google.*/*
 // @grant       none
-// @version     1.1
+// @version     1.2
 // @author      era2010-coder
 // @description 5/20/2021, 12:24:31 PM
 // ==/UserScript==
