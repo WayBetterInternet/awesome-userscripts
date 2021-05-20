5db11ed8c9627a6bbbc7be0e14f923e47aee7618
era2010-coder <72707293+era2010-coder@users.noreply.github.com>
2021-05-20 12:29:27 -0400

    Create BetterGoogle.user.js

---
+++ BetterGoogle.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        Better Google
+// @namespace   awesome-userscripts.github.io
+// @match       *://google.*
+// @grant       none
+// @version     1.0
+// @author      era2010-coder
+// @description 5/20/2021, 12:24:31 PM
+// ==/UserScript==
+
+console.log("Google");
