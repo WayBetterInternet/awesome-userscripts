23416d9635f32951f3af6b5104ae1341cff2cb3a
era2010-coder <72707293+era2010-coder@users.noreply.github.com>
2021-05-20 10:54:50 -0400

    Create BetterGoogleClassroom.user.js

---
+++ BetterGoogleClassroom.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        New script - google.com
+// @namespace   Violentmonkey Scripts
+// @match       *://classroom.google.com/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 5/20/2021, 10:49:50 AM
+// ==/UserScript==
+
+console.log("I detect google classroom")
