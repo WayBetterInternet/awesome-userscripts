2532e8e2dbda12e80e98917eeabb6ac090a210ba
snowcatridge10 <72707293+snowcatridge10@users.noreply.github.com>
2021-11-28 10:55:16 -0500

    Update RedirectFromGoogleToDuckDuckGo.user.js

--- RedirectFromGoogleToDuckDuckGo.user.js
+++ RedirectFromGoogleToDuckDuckGo.user.js
@@ -2,7 +2,7 @@
 // @name         Redirect from Google to DuckDuckGo
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description  Redirect from Google to DuckDuckGo
 // @author       snowcatridge10
 // @match        https://www.google.com/
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
